1c8406f34464070ee2d04303f826013128074eb3
Disha <dishay166@gmail.com>
2023-09-10 15:59:01 +0530

    update button

--- frontend/src/Pages/AddBooks.jsx
+++ frontend/src/Pages/AddBooks.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const AddBooks = () => {
+  const [Data, setDate] = useState({
+    bookname: "",
+    author: "",
+    description: "",
+    price: "",
+    image: "",
+  });
+  const change = (e) => {
+    const { name, value } = e.target;
+    setDate({ ...Data, [name]: value });
+  };
+  const submit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:1000/api/books/add", Data)
+      .then((res) => alert(res.data.message));
+    setDate({
+      bookname: "",
+      author: "",
+      description: "",
+      price: "",
+      image: "",
+    });
+  };
+  console.log(Data);
   return (
     <div
       className="bg-dark d-flex justify-content-center align-items-centers"
@@ -8,38 +34,56 @@ const AddBooks = () => {
     >
       <div className="container p-4">
         <div className="mb-3">
-          <label for="exampleFormControlInput1" class="form-label text-white">
+          <label
+            for="exampleFormControlInput1"
+            className="form-label text-white"
+          >
             Book Name
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Enter Book Name"
+            name="bookname"
+            value={Data.bookname}
+            onChange={change}
           />
         </div>
 
         <div className="mb-3">
-          <label for="exampleFormControlInput1" class="form-label text-white">
+          <label
+            for="exampleFormControlInput1"
+            className="form-label text-white"
+          >
             Author
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Enter Author's Name"
+            name="author"
+            value={Data.author}
+            onChange={change}
           />
         </div>
 
         <div className="mb-3">
-          <label for="exampleFormControlInput1" class="form-label text-white">
+          <label
+            for="exampleFormControlInput1"
+            className="form-label text-white"
+          >
             Description
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Enter Book Description"
+            name="description"
+            value={Data.description}
+            onChange={change}
           />
         </div>
 
@@ -49,24 +93,35 @@ const AddBooks = () => {
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Enter URL Of The Book Image"
+            name="image"
+            value={Data.image}
+            onChange={change}
           />
         </div>
 
         <div className="mb-3">
-          <label for="exampleFormControlInput1" class="form-label text-white">
+          <label
+            for="exampleFormControlInput1"
+            className="form-label text-white"
+          >
             Price
           </label>
           <input
             type="number"
-            class="form-control"
+            className="form-control"
             id="exampleFormControlInput1"
             placeholder="Enter Book's Price"
+            name="price"
+            value={Data.price}
+            onChange={change}
           />
         </div>
-        <button className="btn btn-success">Submit</button>
+        <button className="btn btn-success" onClick={submit}>
+          Submit
+        </button>
       </div>
     </div>
   );
--- frontend/src/components/BooksSection.jsx
+++ frontend/src/components/BooksSection.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 
 const BooksSection = ({ data }) => {
-  console.log(data);
   return (
-    <div
-      className="d-flex justify-content-around align-items-center flex-wrap
-    my-3"
-    >
+    <div className="d-flex justify-content-around align-items-center flex-wrap">
       {data &&
         data.map((item, index) => (
           <div
-            className=""
+            className="m-3"
             style={{
               width: "200px",
               height: "350px",
-              backgroundColor: "gray",
+              border: "1px solid white",
+              borderRadius: "20px",
             }}
           >
             <div>
               <img
-                style={{ width: "200px", height: "210px" }}
+                style={{
+                  width: "200px",
+                  height: "210px",
+                  borderTopLeftRadius: "20px",
+                  borderTopRightRadius: "20px",
+                }}
                 className="img-fluid"
                 src={item.image}
                 alt="image"
               />
             </div>
-            <h6 style={{ fontSize: "15px" }} className="px-2 m-0">
+            <h6 style={{ fontSize: "15px" }} className="px-2 my-1 text-white">
               {item.bookname.slice(0, 20)}...
             </h6>
-            <p style={{ fontSize: "30px" }} className="m-0 px-2">
+            <b style={{ fontSize: "30px", color: "red" }} className="mb-2 px-2">
               Rs.{item.price}{" "}
-            </p>
+            </b>
+            <div className="d-flex justify-content-around align-items-center">
+              <button className="btn btn-primary">UPDATE</button>
+              <button className="btn btn-danger">DELETE</button>
+            </div>
           </div>
         ))}
     </div>
